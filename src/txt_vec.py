19e9bee16f28174df0b6c7f526de53b886d20532
honehone <honehone.41games@gmail.com>
2025-05-27 16:36:12 +0900

    added assertion checks

---
+++ src/txt_vec.py
@@ -0,0 +1,48 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+from pymongo.database import Database
+from pymongo.collection import Collection
+from pymongo.cursor import Cursor
+import lib.jsonio as jsonio
+from lib.logger import log, loginit
+from lib.documents import Doc, Img, Done
+from lib.logger import log
+import lib.mongo as mongo
+
+async def text_vec():
+    mongo.connect()
+    db = mongo.db('DATABASE')
+    colle: Collection[Doc] = mongo.colle(db, 'COLLECTION')
+
+    img_list = [Img(**img) for img in jsonio.parse('IMG_LIST')]
+    done_list = [Done(**done) for done in jsonio.parse('DONE_LIST')]
+
+    stream: Cursor[Doc] = colle.find({})
+    async for doc in stream:
+        done_found = [d for d in done_list if d.item_id == doc.item_id]
+        l = len(done_found)
+        if l > 1:
+            raise AssertionError(f'{l} same item id found in done list')
+        elif l == 1:
+            continue
+        
+        img_found = [i for i in img_list if i.item_id == doc.item_id]
+        l = len(img_found)
+        if l > 1:
+            raise AssertionError(f'{l} same item id found in img list')
+        elif l == 0:
+            res = await colle.delete_one({_id: doc._id})
+            log().warn(f'deleted {res.deleted_count} item without img')
+
+        desc = doc.description
+        log().info(desc)
+
+if __name__ == '__main__':
+    loginit(__name__)
+    load_dotenv()
+    
+    try:
+        asyncio.run(text_vec())
+    except Exception as e:
+        log().error(e)
