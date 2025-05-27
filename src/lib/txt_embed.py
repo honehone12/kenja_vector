89d1977e28b66ee59ed828662d80d70a56171fd6
honehone <honehone.41games@gmail.com>
2025-05-27 18:32:19 +0900

    added txt embed

---
+++ src/lib/txt_embed.py
@@ -0,0 +1,22 @@
+import os
+from sentence_transformers import SentenceTransformer
+from torch import Tensor
+
+__PROMPT = 'passage'
+
+__model = None
+
+def ini_model():
+    global __model
+
+    model_name = os.getenv('TXT_EMBED_MODEL')
+    if model_name is None:
+        raise ValueError('env for TEXT_EMBED_MODEL is not set')
+
+    __model = SentenceTransformer(model_name, trust_remote_code=True)
+
+def vector(sentence: str) -> Tensor:
+    if __model is None:
+        raise ValueError('model is not initialized')
+
+    return __model.encode(sentence, prompt_name=__PROMPT)
