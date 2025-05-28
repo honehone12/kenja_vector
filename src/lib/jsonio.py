9440d347666845907162aa36a904167e3832c0fe
honehone <honehone.41games@gmail.com>
2025-05-28 18:51:13 +0900

    fixed io

--- src/lib/jsonio.py
+++ src/lib/jsonio.py
@@ -10,15 +10,16 @@ def parse(dir_key: str, file_name: str):
         with open(file_name, 'r') as file:
             return json.load(file)
     else:
-        return []
+        with open(file_name, 'x') as file:
+            return []
 
 def save(dir_key: str, file_name: str, data: any):
     json_root = os.getenv(dir_key)
     if json_root is None:
         raise ValueError(f'env for {dir_key} is not set')
+
+    mode = 'w'
     if not os.path.exists(file_name):
-        with open(file_name, 'x') as file:
-            json.dump(data, file, indent=2)
-    else:
-        with open(file_name, 'w') as file:
-            json.dump(data, file, indent=2)
+        mode = 'x'
+    with open(file_name, mode) as file:
+        json.dump(data, file, indent=2)
