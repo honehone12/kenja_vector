08171792966c2e351cbb7ca3a711893ea7388366
honehone <honehone.41games@gmail.com>
2025-06-05 20:21:27 +0900

    removed unused class

--- src/lib/documents.py
+++ src/lib/documents.py
@@ -1,21 +1,5 @@
-from typing import TypedDict #, override
+from typing import TypedDict
 from bson import ObjectId
-
-ITEM_T_ANIME = 1
-ITEM_T_CHARACTER = 2
-
-class ItemId(TypedDict):
-    id: int
-    item_type: int
-
-    # @override
-    # def __eq__(self, other) -> bool:
-    #     if isinstance(other, ItemId):
-    #         return self.item_type == other.item_type and self.id == other.id
-    #     elif isinstance(other, dict):
-    #         return self.item_type == other['item_type'] and self.id == other['id']
-    #     else:
-    #         raise ValueError('unexpected instance type')
 
 class Doc(TypedDict):
     _id: ObjectId
