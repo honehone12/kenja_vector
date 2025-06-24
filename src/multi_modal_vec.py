a54719b700a9b9c5c88da74535aa58675fe23767
Twelfthonehone <honehone.41games@gmail.com>
2025-06-24 13:27:02 +0900

    fixed skipping

--- src/multi_modal_vec.py
+++ src/multi_modal_vec.py
@@ -1,33 +1,50 @@
 import asyncio
 import os
 from urllib.parse import urlparse
+from bson import ObjectId
 from dotenv import load_dotenv
+from pymongo import DeleteOne, UpdateOne
 from pymongo.asynchronous.collection import AsyncCollection
 from pymongo.asynchronous.cursor import AsyncCursor
 from lib import mongo
 from lib.documents import IMG_VEC_FIELD, TXT_VEC_FIELD, STF_VEC_FIELD, Doc
 from lib.multi_modal_embed import init_multi_modal_model, img_vector, txt_vector
 
-async def img(img_root: str, url: str):
+async def img(img_root: str, url: str, id: ObjectId):
     if len(url) == 0:
-        raise ValueError('null url')
+        raise ValueError('empty url')
 
     path = img_root + urlparse(url).path.removesuffix('/')
     if not os.path.exists(path):
         print(f'image not found {path}')
-        return
+        d = DeleteOne(
+            filter={'_id': id}
+        )
+        return d, False
 
     v = img_vector(path)
-    print(v.shape)
+    b = mongo.compress_bin(v)
+    u = UpdateOne(
+        filter={'_id': id},
+        update={
+            '$set': {IMG_VEC_FIELD: b},
+            '$unset': {'img': ''}
+        }
+    )
+    return u, True
 
-async def txt(text: str):
+async def txt(field: str, text: str, id: ObjectId):
     if len(text) == 0:
-        print('null text')
-        return
+        raise ValueError('empty text')
 
     v = txt_vector(text)
-    print(v.shape)
-
+    b = mongo.compress_bin(v)
+    u = UpdateOne(
+        filter={'_id': id},
+        update={'$set': {field: b}}
+    )
+    return u
+    
 
 async def multi_modal_vec(iteration: int, batch_size: int, img_root: str):
     db = mongo.db('DATABASE')
@@ -35,17 +52,28 @@ async def multi_modal_vec(iteration: int, batch_size: int, img_root: str):
     stream: AsyncCursor[Doc] = cl.find({})
 
     it = 0
-    total = 0
     batch = []
     async for doc in stream:
-        if doc.get(IMG_VEC_FIELD) is None:
-            await img(img_root, doc['img'])
+        id = doc['_id']
 
+        if doc.get(IMG_VEC_FIELD) is None:
+            op, ok = await img(img_root, doc['img'], id)
+            batch.append(op)
+            if not ok:
+                continue
+            
         if doc.get(TXT_VEC_FIELD) is None:
-            await txt(doc['description'])
+            op = await txt(TXT_VEC_FIELD, doc['description'], id)
+            batch.append(op)
 
         if doc.get(STF_VEC_FIELD) is None:
-            await txt(doc['staff'])
+            op = await txt(STF_VEC_FIELD, doc['staff'], id)
+            batch.append(op)
+
+        if len(batch) >= batch_size:
+            res = await cl.bulk_write(batch)
+            print(f'{res.modified_count} updated')
+            batch.clear()
 
         it += 1
         print(f'iteration {it} done')
@@ -53,6 +81,9 @@ async def multi_modal_vec(iteration: int, batch_size: int, img_root: str):
             print('iteration limit')
             break
 
+    if len(batch) > 0:
+        res = await cl.bulk_write(batch)
+        print(f'{res.modified_count} updated')
     print('done')
 
 if __name__ == '__main__':
@@ -66,7 +97,7 @@ if __name__ == '__main__':
             iteration = int(itenv)
 
         batchenv = os.getenv('BATCH_SIZE')
-        batch_size = 200
+        batch_size = 100
         if batchenv is not None:
             batch_size = int(batchenv)
 
