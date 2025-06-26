fea523c199b623e490f2b99e61a6a67a0c20a9b2
Twelfthonehone <honehone.41games@gmail.com>
2025-06-26 10:50:45 +0900

    added shape view

--- src/shape.py
+++ src/shape.py
@@ -12,70 +12,43 @@ from lib.documents import IMG_VEC_FIELD, TXT_VEC_FIELD, STF_VEC_FIELD, Doc
 # from lib.image_tsfm import init_image_tsfm_model, image_vector
 from lib.colpali import init_colpali_model, image_vector, sentence_vector
 
-def process_image(img_root: str, url: str, id: ObjectId):
+def process_image(img_root: str, url: str):
     if len(url) == 0:
         raise ValueError('empty url')
 
     path = img_root + urlparse(url).path.removesuffix('/')
     if not os.path.exists(path):
         print(f'image not found {path}')
-        d = DeleteOne(
-            filter={'_id': id}
-        )
-        return d, False
+        return
 
     v = image_vector(path)
-    b = mongo.compress_bin(v)
-    u = UpdateOne(
-        filter={'_id': id},
-        update={
-            '$set': {IMG_VEC_FIELD: b},
-            '$unset': {'img': ''}
-        }
-    )
-    return u, True
+    print(v.shape)
 
-def process_text(field: str, text: str, id: ObjectId):
+def process_text(text: str):
     if len(text) == 0:
         raise ValueError('empty text')
 
     v = sentence_vector(text)
-    b = mongo.compress_bin(v)
-    u = UpdateOne(
-        filter={'_id': id},
-        update={'$set': {field: b}}
-    )
-    return u
+    print(v.shape)
     
 
-async def process_vecs(iteration: int, batch_size: int, img_root: str):
+async def process_vecs(iteration: int, img_root: str):
     db = mongo.db('DATABASE')
     cl: AsyncCollection[Doc] = mongo.collection(db, 'COLLECTION')
     stream: AsyncCursor[Doc] = cl.find({})
 
     it = 0
-    batch = []
     async for doc in stream:
         id = doc['_id']
 
         if doc.get(IMG_VEC_FIELD) is None:
-            op, ok = process_image(img_root, doc['img'], id)
-            batch.append(op)
-            if not ok:
-                continue
+            process_image(img_root, doc['img'])
             
         if doc.get(TXT_VEC_FIELD) is None:
-            op = process_text(TXT_VEC_FIELD, doc['description'], id)
-            batch.append(op)
+            process_text(doc['description'])
 
         if doc.get(STF_VEC_FIELD) is None:
-            op = process_text(STF_VEC_FIELD, doc['staff'], id)
-            batch.append(op)
-
-        if len(batch) >= batch_size:
-            res = await cl.bulk_write(batch)
-            print(f'{res.modified_count} updated')
-            batch.clear()
+            process_text(doc['staff'])
 
         it += 1
         print(f'iteration {it} done')
@@ -83,9 +56,6 @@ async def process_vecs(iteration: int, batch_size: int, img_root: str):
             print('iteration limit')
             break
 
-    if len(batch) > 0:
-        res = await cl.bulk_write(batch)
-        print(f'{res.modified_count} updated')
     print('done')
 
 if __name__ == '__main__':
@@ -99,7 +69,6 @@ if __name__ == '__main__':
             iteration = int(itenv)
 
         batchenv = os.getenv('BATCH_SIZE')
-        batch_size = 100
         if batchenv is not None:
             batch_size = int(batchenv)
 
@@ -111,6 +80,6 @@ if __name__ == '__main__':
         # init_image_tsfm_model()
         init_colpali_model()
         mongo.connect()
-        asyncio.run(process_vecs(iteration, batch_size, img_root))
+        asyncio.run(process_vecs(iteration, img_root))
     except Exception as e:
         print(e)
